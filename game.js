c16dc59990cbf5ec0639c7ba8711ee4323a9e554
Alexey Volkov <grulfex@gmail.com>
2019-11-13 21:56:10 +0300

    Add logic that shows game history in a div

--- game.js
+++ game.js
@@ -9,56 +9,40 @@ function computerPlay() {
     }
 }
 
-function playerPlay() {
-    var playerSelection = prompt("Choose rock, paper or scissors.").toLowerCase();
-    if(playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
-        return playerSelection;
-    } else {
-        console.log("Sorry, but you seem to have mistyped that, try again.");
-        return playerPlay()
-    }
+function startGame() {
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => button.addEventListener('click', playerPlay));
+};
+
+function playerPlay(){
+    var playerSelection = this.id;
+    var roundResult = playRound(playerSelection);
+    renderDiv(roundResult);
 }
 
-function game() {
-    var i = 1;
-    var playerScore = 0;
-    var computerScore = 0;
-    while (i<6 || playerScore==computerScore) {
-        if(i >= 6) {
-            console.log("Equal points after 5th round. Playing another one!");
-        }
-        console.log("Round " + i);
-        var roundResult = playRound(playerScore, computerScore);
-        console.log(roundResult[0])
-        playerScore = roundResult[1];
-        computerScore = roundResult[2];
-        i++;
-    };
-    if(playerScore > computerScore) {
-        return "You win " + playerScore + ":" + computerScore + "!"
-    } else {
-        return "You lose " + playerScore + ":" + computerScore + "!"
-    }
-}
-
-function playRound(playerScore, computerScore) {
+function playRound(playerSelection) {
     var computerSelection = computerPlay();
-    var playerSelection =  playerPlay();
     var playerWins = ((playerSelection === "rock") && (computerSelection === "scissors")) ||
         ((playerSelection === "paper") && (computerSelection === "rock")) ||
         ((playerSelection === "scissors") && (computerSelection === "paper"));
     var itIsADraw = playerSelection === computerSelection;
     if (playerWins) {
-        playerScore++;
         return ["You win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + 
-            " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1), playerScore, computerScore];
+            " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)];
     } else if (itIsADraw) {
-        return ["It is a draw!", playerScore, computerScore]
+        return ["It is a draw!"]
     } else {
-        computerScore++;
         return ["You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + 
-            " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1), playerScore, computerScore];
+            " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)];
     }
 }
 
-console.log(game());
+function renderDiv(roundResult){
+    const container = document.querySelector('#container');
+    const resultsDiv = document.createElement('div');
+    resultsDiv.classList.add('round-result');
+    resultsDiv.textContent = roundResult[0];
+    container.appendChild(resultsDiv);
+}
+
+startGame();
