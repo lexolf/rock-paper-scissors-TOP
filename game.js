f254d4290bee3c4bcefaf16bf3b51bb65994b58b
Alexey Volkov <grulfex@gmail.com>
2019-11-29 19:44:46 +0300

    Add counter and 5 rounds

--- game.js
+++ game.js
@@ -21,17 +21,30 @@ function playerPlay(){
 }
 
 function playRound(playerSelection) {
+    playerScoreSpan = document.getElementById("player-score");
+    computerScoreSpan = document.getElementById("computer-score");
+    var playerScore = playerScoreSpan.textContent;
+    var computerScore = computerScoreSpan.textContent;
+    if(playerScore == 5){
+        return ["You win the game!"]
+    } else if (computerScore == 5){
+        return ["Computer wins the game!"]
+    }
     var computerSelection = computerPlay();
     var playerWins = ((playerSelection === "rock") && (computerSelection === "scissors")) ||
         ((playerSelection === "paper") && (computerSelection === "rock")) ||
         ((playerSelection === "scissors") && (computerSelection === "paper"));
     var itIsADraw = playerSelection === computerSelection;
     if (playerWins) {
+        playerScore++;
+        playerScoreSpan.textContent = playerScore;
         return ["You win! " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + 
             " beats " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)];
     } else if (itIsADraw) {
         return ["It is a draw!"]
     } else {
+        computerScore++;
+        computerScoreSpan.textContent = computerScore;
         return ["You lose! " + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + 
             " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)];
     }
